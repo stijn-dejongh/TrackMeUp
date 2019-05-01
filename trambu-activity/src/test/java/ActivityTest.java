a211c852e2a5afbb3395971b66174e78aee99300
Doji <stijn.c.dejongh@gmail.com>
2019-05-01 14:14:59 +0200

    Updated test and code, decided to not bother with LocalDateTime parsing untill I needed to...

--- trambu-activity/src/test/java/ActivityTest.java
+++ trambu-activity/src/test/java/ActivityTest.java
@@ -1,16 +1,20 @@
-import java.time.LocalDate;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.text.ParseException;
+import java.time.LocalDateTime;
+import org.junit.Test;
 
 public class ActivityTest {
 
-  public static void plan_activity_at_certain_date() {
+  @Test
+  public void activity_builder_creates_object() throws ParseException {
     Activity activity = Activity.builder()
         .name("Start design practise")
-        .plannedStartAt("01-05-2019")
-        .plannedEndAt("31-05-2019");
+        .plannedStartAt(LocalDateTime.of(2019, 05, 01, 11, 00))
+        .plannedEndAt(LocalDateTime.of(2019, 05, 31, 18, 00))
+        .build();
 
-    assertThat(activity.getAllocatedTimeslot().getStart()).isSameDay(LocalDate.of(2019, 05, 01));
-    assertThat(activity.getAllocatedTimeslot().getEnd()).isSameDay(LocalDate.of(2019, 05, 31));
-
+    assertThat(activity).isNotNull();
   }
 
 }
